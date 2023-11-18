52add749050d4faedff0f20456781fb0c2930eca
cloudgae <jisu5868@gmail.com>
2023-11-18 22:12:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/searchaddressexample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.searchaddressexample;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEtAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEtAddress = findViewById(R.id.et_address);
+
+        mEtAddress.setFocusable(false);
+        mEtAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //주소 검색 웹뷰 화면으로 이동
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                getSearchResult.launch(intent);
+            }
+        });
+    }
+
+    private final ActivityResultLauncher<Intent> getSearchResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                //Search Activity로부터의 결과값이 이곳으로 전달됨
+                if (result.getResultCode() == RESULT_OK){
+                    if (result.getData() != null){
+                        String data = result.getData().getStringExtra("data");
+                        mEtAddress.setText(data);
+                    }
+                }
+            }
+    );
+}
